23d94b0b5c65593b99bad4642e0060b3aa3dc466
David Sedej <skejterkelly@gmail.com>
2024-01-23 14:44:00 +0100

    commit #5

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,13 @@
 function dropDown() {
+    burger = document.getElementById("burgerID");
     drop_down = document.querySelector(".drop_down");
     if (drop_down.style.transform == "translateX(100vw)") {
         drop_down.style.transform = "translateX(65vw)";
+        burger.classList.remove("burger");
+        burger.classList.add("burger_cross");
     } else {
         drop_down.style.transform = "translateX(100vw)";
+        burger.classList.remove("burger_cross");
+        burger.classList.add("burger");
     }
 }
